4bb6c277812af05bcbdb48427ba9f93dc345d163
Youmir <s.youmir@gmail.com>
2020-02-09 00:36:18 +0100

    Registration and loggin forms

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, url_for, flash,redirect
+from forms import RegistrationForm, LogForm
+import os
 
 
 app = Flask(__name__)
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
 
 posts = [
     {
@@ -26,6 +31,25 @@ def home():
 def about():
     return render_template('about.html',title='About blog')
 
+@app.route('/register',methods =['POST','GET'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title='Register', form=form)
+
+@app.route('/login',methods =['POST','GET'])
+def login():
+    form = LogForm()
+    if form.validate_on_submit():
+        if form.email.data == "s.youmir@gmail.com" and form.password.data == "password":
+            flash('You have been logged in!','success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful','danger')
+        
+    return render_template('login.html',title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
